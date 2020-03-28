4313fc5a469b42be6a1e76b3f3cfaf7f404aa655
Richard Artoul <richard.artoul@datadoghq.com>
2020-03-28 17:37:37 -0400

    add doc strings

--- src/molecule.go
+++ src/molecule.go
@@ -8,8 +8,12 @@ import (
 	"github.com/richardartoul/molecule/src/codec"
 )
 
+// MessageEachFn is a function that will be called for each top-level field in a
+// message passed to MessageEach.
 type MessageEachFn func(fieldNum int32, value Value) error
 
+// MessageEach iterates over each top-level field in message b and calls fn on
+// each one.
 func MessageEach(b []byte, fn MessageEachFn) error {
 	buffer := codec.NewBuffer(b)
 	for !buffer.EOF() {
@@ -30,9 +34,14 @@ func MessageEach(b []byte, fn MessageEachFn) error {
 	return nil
 }
 
-type ArrayEachFn func(value Value) error
+// PackedRepeatedEachFn is a function that is called for each value in a repeated field.
+type PackedRepeatedEachFn func(value Value) error
 
-func PackedArrayEach(b []byte, wireType int8, fn ArrayEachFn) error {
+// PackedArrayEach iterates over each value in packed repeated field b and calls fn on
+// each one.
+//
+// PackedArrayEach only supports repeated fields encoded using packed encoding.
+func PackedArrayEach(b []byte, wireType int8, fn PackedRepeatedEachFn) error {
 	buffer := codec.NewBuffer(b)
 
 	for !buffer.EOF() {
@@ -94,6 +103,9 @@ func readValueFromBuffer(wireType int8, buffer *codec.Buffer) (Value, error) {
 	return value, nil
 }
 
+// Value represents a protobuf value. It contains the original wiretype that the value
+// was encoded with as well as a variety of helper methods for interpreting the raw
+// value based on the field's actual type.
 type Value struct {
 	// WireType is the protobuf wire type that was used to encode the field.
 	WireType int8
@@ -111,10 +123,12 @@ type Value struct {
 	Bytes []byte
 }
 
+// AsDouble interprets the value as a double.
 func (v *Value) AsDouble() (float64, error) {
 	return math.Float64frombits(v.Number), nil
 }
 
+// AsFloat interprets the value as a float.
 func (v *Value) AsFloat() (float32, error) {
 	if v.Number > math.MaxUint32 {
 		return 0, fmt.Errorf("AsFloat: %d overflows float32", v.Number)
@@ -122,6 +136,7 @@ func (v *Value) AsFloat() (float32, error) {
 	return math.Float32frombits(uint32(v.Number)), nil
 }
 
+// AsInt32 interprets the value as an int32.
 func (v *Value) AsInt32() (int32, error) {
 	s := int64(v.Number)
 	if s > math.MaxInt32 {
@@ -133,10 +148,12 @@ func (v *Value) AsInt32() (int32, error) {
 	return int32(v.Number), nil
 }
 
+// AsInt64 interprets the value as an int64.
 func (v *Value) AsInt64() (int64, error) {
 	return int64(v.Number), nil
 }
 
+// AsUint32 interprets the value as a uint32.
 func (v *Value) AsUint32() (uint32, error) {
 	if v.Number > math.MaxUint32 {
 		return 0, fmt.Errorf("AsUInt32: %d overflows uint32", v.Number)
@@ -144,10 +161,12 @@ func (v *Value) AsUint32() (uint32, error) {
 	return uint32(v.Number), nil
 }
 
+// AsUint64 interprets the value as a uint64.
 func (v *Value) AsUint64() (uint64, error) {
 	return v.Number, nil
 }
 
+// AsSint32 interprets the value as a sint32.
 func (v *Value) AsSint32() (int32, error) {
 	if v.Number > math.MaxUint32 {
 		return 0, fmt.Errorf("AsSint32: %d overflows int32", v.Number)
@@ -155,10 +174,12 @@ func (v *Value) AsSint32() (int32, error) {
 	return codec.DecodeZigZag32(v.Number), nil
 }
 
+// AsSint64 interprets the value as a sint64.
 func (v *Value) AsSint64() (int64, error) {
 	return codec.DecodeZigZag64(v.Number), nil
 }
 
+// AsFixed32 interprets the value as a fixed32.
 func (v *Value) AsFixed32() (uint32, error) {
 	if v.Number > math.MaxUint32 {
 		return 0, fmt.Errorf("AsFixed32: %d overflows int32", v.Number)
@@ -166,10 +187,12 @@ func (v *Value) AsFixed32() (uint32, error) {
 	return uint32(v.Number), nil
 }
 
+// AsFixed64 interprets the value as a fixed64.
 func (v *Value) AsFixed64() (uint64, error) {
 	return uint64(v.Number), nil
 }
 
+// AsSFixed32 interprets the value as a SFixed32.
 func (v *Value) AsSFixed32() (int32, error) {
 	if v.Number > math.MaxUint32 {
 		return 0, fmt.Errorf("AsSFixed32: %d overflows int32", v.Number)
@@ -177,19 +200,23 @@ func (v *Value) AsSFixed32() (int32, error) {
 	return int32(v.Number), nil
 }
 
+// AsSFixed64 interprets the value as a SFixed64.
 func (v *Value) AsSFixed64() (int64, error) {
 	return int64(v.Number), nil
 }
 
+// AsBool interprets the value as a bool.
 func (v *Value) AsBool() (bool, error) {
 	return v.Number == 1, nil
 }
 
+// AsString interprets the value as a string.
 func (v *Value) AsString() (string, error) {
 	// TODO: Do unsafe conversion here.
 	return string(v.Bytes), nil
 }
 
+// AsBytes interprets the value as bytes.
 func (v *Value) AsBytes() ([]byte, error) {
 	return v.Bytes, nil
 }

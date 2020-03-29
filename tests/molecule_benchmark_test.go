93903d147a61a357b9d18dad7daeb7e648ef1da3
Richard Artoul <richard.artoul@datadoghq.com>
2020-03-29 14:13:06 -0400

    Improve benchmark

--- tests/molecule_benchmark_test.go
+++ tests/molecule_benchmark_test.go
@@ -13,13 +13,12 @@ import (
 )
 
 func BenchmarkMolecule(b *testing.B) {
-	b.StopTimer()
 	var (
 		seed   = time.Now().UnixNano()
 		fuzzer = fuzz.NewWithSeed(seed)
 	)
 	// Limit slice size to prevent tests from taking too long.
-	fuzzer.NumElements(0, 10)
+	fuzzer.NumElements(0, 100)
 	fuzzer.NilChance(0)
 
 	m := &simple.Simple{}
@@ -27,42 +26,69 @@ func BenchmarkMolecule(b *testing.B) {
 	marshaled, err := proto.Marshal(m)
 	noErr(err)
 
-	var (
-		msgBuffer   = codec.NewBuffer(marshaled)
-		arrayBuffer = codec.NewBuffer(nil)
-		int64s      []int64
-	)
-	b.StartTimer()
-	for i := 0; i < b.N; i++ {
-		msgBuffer.Reset(marshaled)
-		int64s = int64s[:0]
+	b.Run("standard unmarshal", func(b *testing.B) {
+		into := &simple.Simple{}
+		for i := 0; i < b.N; i++ {
+			err := proto.Unmarshal(marshaled, into)
+			noErr(err)
+		}
+	})
 
-		err := molecule.MessageEach(msgBuffer, func(fieldNum int32, value molecule.Value) bool {
-			switch fieldNum {
-			case 14:
-				_, err := value.AsStringUnsafe()
-				noErr(err)
-			case 15:
-				_, err := value.AsBytesUnsafe()
-				noErr(err)
-			case 16:
-				packedArr, err := value.AsBytesUnsafe()
-				noErr(err)
-
-				arrayBuffer.Reset(packedArr)
-				err = molecule.PackedRepeatedEach(arrayBuffer, codec.FieldType_INT64, func(value molecule.Value) bool {
-					v, err := value.AsInt64()
+	b.Run("unmarshal single with molecule", func(b *testing.B) {
+		msgBuffer := codec.NewBuffer(marshaled)
+		for i := 0; i < b.N; i++ {
+			msgBuffer.Reset(marshaled)
+			err := molecule.MessageEach(msgBuffer, func(fieldNum int32, value molecule.Value) bool {
+				switch fieldNum {
+				case 14:
+					_, err := value.AsStringUnsafe()
 					noErr(err)
-					int64s = append(int64s, v)
-					return true
-				})
-				noErr(err)
-			}
+					return false
+				}
 
-			return true
-		})
-		noErr(err)
-	}
+				return true
+			})
+			noErr(err)
+		}
+	})
+
+	b.Run("unmarshal multiple with molecule", func(b *testing.B) {
+		var (
+			msgBuffer   = codec.NewBuffer(marshaled)
+			arrayBuffer = codec.NewBuffer(nil)
+			int64s      []int64
+		)
+		for i := 0; i < b.N; i++ {
+			msgBuffer.Reset(marshaled)
+			int64s = int64s[:0]
+
+			err := molecule.MessageEach(msgBuffer, func(fieldNum int32, value molecule.Value) bool {
+				switch fieldNum {
+				case 14:
+					_, err := value.AsStringUnsafe()
+					noErr(err)
+				case 15:
+					_, err := value.AsBytesUnsafe()
+					noErr(err)
+				case 16:
+					packedArr, err := value.AsBytesUnsafe()
+					noErr(err)
+
+					arrayBuffer.Reset(packedArr)
+					err = molecule.PackedRepeatedEach(arrayBuffer, codec.FieldType_INT64, func(value molecule.Value) bool {
+						v, err := value.AsInt64()
+						noErr(err)
+						int64s = append(int64s, v)
+						return true
+					})
+					noErr(err)
+				}
+
+				return true
+			})
+			noErr(err)
+		}
+	})
 }
 
 func noErr(err error) {
